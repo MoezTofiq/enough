486a32b5b1ebb1718931376f4a835df0b9ec2b9e
Moeez Tofiq <moeeztofiq@gmail.com>
2024-07-30 15:44:39 +0500

    added icon on loading screen

--- src/Components/ImproveSettings.tsx
+++ src/Components/ImproveSettings.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import HistoryToggleOffIcon from "@mui/icons-material/HistoryToggleOff";
 
 export const ImproveSettings = () => {
   return (
@@ -27,6 +28,22 @@ export const ImproveSettings = () => {
           <Typography variant="h6" color="primary" textAlign={"center"}>
             By not loading sections on the screen
           </Typography>
+        </Grid2>
+        <Grid2
+          xs={12}
+          sm={12}
+          md={12}
+          lg={12}
+          xl={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection={"column"}
+        >
+          <HistoryToggleOffIcon
+            color="primary"
+            sx={{ zIndex: 0, fontSize: 180 }}
+          />
         </Grid2>
       </Grid2>
     </Box>
